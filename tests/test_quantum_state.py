fdf9807338099283e3c88a78025c8a30117c554d
Slope <60516263+Slope86@users.noreply.github.com>
2024-07-18 15:55:23 +0800

    refactor: Update QuantumState import and variable names for clarity

--- tests/test_quantum_state.py
+++ tests/test_quantum_state.py
@@ -3,8 +3,8 @@ import math
 import numpy as np
 from numpy.typing import NDArray
 
-from QIRT.quantum_state import QuantumState as qs
-from QIRT.utils.ket import Ket
+from QIRT import QuantumState
+from QIRT.utils import Ket
 
 ABS_TOL = 1e-15  # absolute tolerance for close comparisons
 
@@ -18,20 +18,30 @@ Y1 = Ket.y1
 
 def test_from_label():
     # (|00> + |11>) / sqrt(2)
-    stateBellZ = qs.from_label((Z0 + Z0), (Z1 + Z1))
+    Bell_state_Z = QuantumState.from_label((Z0 + Z0), (Z1 + Z1))
     # (|++> + |-->) / sqrt(2)
-    stateBellX = qs.from_label((X0 + X0), (X1 + X1))
+    Bell_state_X = QuantumState.from_label((X0 + X0), (X1 + X1))
 
     expected_state_data = np.array([0.70710678 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.70710678 + 0.0j])
-    assert np.allclose(stateBellZ.data, expected_state_data, atol=ABS_TOL)
-    assert np.allclose(stateBellX.data, expected_state_data, atol=ABS_TOL)
+    assert np.allclose(Bell_state_Z.data, expected_state_data, atol=ABS_TOL)
+    assert np.allclose(Bell_state_X.data, expected_state_data, atol=ABS_TOL)
 
-    # (|+0> + |-0>) / sqrt(2)
+    # (|0+> + |1->) / sqrt(2)
+    graph_state = QuantumState.from_label((Z0 + X0), (Z1 + X1))
+
+    expected_state_data = np.array([0.5 + 0.0j, 0.5 + 0.0j, 0.5 + 0.0j, -0.5 + 0.0j])
+    assert np.allclose(graph_state.data, expected_state_data, atol=ABS_TOL)
+
+    # (|0i> + |1j>) / sqrt(2)
+    state = QuantumState.from_label((Z0 + Y0), (Z1 + Y1))
+
+    expected_state_data = np.array([0.5 + 0.0j, 0.0 + 0.5j, 0.5 + 0.0j, 0.0 - 0.5j])
+    assert np.allclose(state.data, expected_state_data, atol=ABS_TOL)
 
 
 def test_entropy():
     # |000>
-    state = qs.from_label(Z0 * 3)
+    state = QuantumState.from_label(Z0 * 3)
     assert math.isclose(state.entropy(), 0.0, abs_tol=ABS_TOL)
 
     # |0+0> + |0-0>
@@ -52,36 +62,48 @@ def test_to_matrix():
     expect_state_vector: NDArray
 
     # |000>
-    test_state_vector = qs.from_label(Z0 * 3).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z0 * 3).to_matrix().squeeze()
     expect_state_vector = np.array([1, 0, 0, 0, 0, 0, 0, 0])
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
 
     # |01>
-    test_state_vector = qs.from_label(Z0 + Z1).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z0 + Z1).to_matrix().squeeze()
     expect_state_vector = np.array([0, 1, 0, 0])
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
 
     # |0+>
-    test_state_vector = qs.from_label(Z0 + X0).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z0 + X0).to_matrix().squeeze()
     expect_state_vector = np.array([1, 1, 0, 0]) / math.sqrt(2)
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
 
     # |0->
-    test_state_vector = qs.from_label(Z0 + X1).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z0 + X1).to_matrix().squeeze()
     expect_state_vector = np.array([1, -1, 0, 0]) / math.sqrt(2)
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
 
     # |1+>
-    test_state_vector = qs.from_label(Z1 + X0).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z1 + X0).to_matrix().squeeze()
     expect_state_vector = np.array([0, 0, 1, 1]) / math.sqrt(2)
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
 
     # |1->
-    test_state_vector = qs.from_label(Z1 + X1).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z1 + X1).to_matrix().squeeze()
     expect_state_vector = np.array([0, 0, 1, -1]) / math.sqrt(2)
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
 
     # |0++> + |1-->
-    test_state_vector = qs.from_label(Z0 + X0 + X0, Z1 + X1 + X1).to_matrix().squeeze()
+    test_state_vector = QuantumState.from_label(Z0 + X0 + X0, Z1 + X1 + X1).to_matrix().squeeze()
     expect_state_vector = np.array([1, 1, 1, 1, 1, -1, -1, 1]) / math.sqrt(8)
     assert np.allclose(test_state_vector, expect_state_vector, atol=ABS_TOL)
+
+    # |i>
+    test_state_vector = QuantumState.from_label(Y0).to_matrix().squeeze()
+    expect_state_vector = np.array([1 + 0j, 0 + 1j]) / math.sqrt(2)
+
+    # |j>
+    test_state_vector = QuantumState.from_label(Y1).to_matrix().squeeze()
+    expect_state_vector = np.array([1 + 0j, 0 - 1j]) / math.sqrt(2)
+
+    # |0i> + |1j>
+    test_state_vector = QuantumState.from_label(Z0 + Y0, Z1 + Y1).to_matrix().squeeze()
+    expect_state_vector = np.array([1 + 0j, 0 + 1j, 1 + 0j, 0 - 1j]) / 2
