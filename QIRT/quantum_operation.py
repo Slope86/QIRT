d76917c5abe704b0c53f0c6e4edf814df8359140
Slope <60516263+Slope86@users.noreply.github.com>
2024-07-18 15:54:30 +0800

    Refactor: Switch from Inheritance to Composition for QuantumOperation
    - Refactored QuantumOperation class to use composition instead of inheritance.
    - Moved extended functionalities (to_matrix, draw, xyz_convert_circ, apply_unitary) into the QuantumOperation class.
    - QuantumOperation now takes a QuantumCircuit instance and extends its functionalities without inheriting from it.
    - This change improves code modularity and adheres to the composition over inheritance principle, making the codebase more maintainable and flexible.

--- QIRT/quantum_operation.py
+++ QIRT/quantum_operation.py
@@ -11,19 +11,27 @@ The functionalities added in this module are:
 - Conversion of the quantum circuit to a matrix form.\n
 - Drawing the quantum circuit or its matrix representation.\n
 - Adding gates to convert qubits from one basis to another.\n
+- Applying unitary matrices to specified qubits.\n
 
 See Also:
     [Qiskit QuantumCircuit documentation](https://qiskit.org/documentation/stubs/qiskit.circuit.QuantumCircuit.html)
 """
 
+from __future__ import annotations
+
+from typing import Sequence
+
 import numpy as np
 from numpy.typing import NDArray
 from qiskit import QuantumCircuit, quantum_info
+from qiskit.circuit.instructionset import InstructionSet
+from qiskit.circuit.quantumcircuit import QubitSpecifier
 
 from QIRT import latex_drawer
+from QIRT.utils import inverse_tensor
 
 
-class QuantumOperation(QuantumCircuit):
+class QuantumOperation:
     """An extended class of QuantumCircuit from Qiskit.
 
     This class extends the QuantumCircuit class from Qiskit to provide additional
@@ -34,6 +42,10 @@ class QuantumOperation(QuantumCircuit):
         [Qiskit QuantumCircuit documentation](https://qiskit.org/documentation/stubs/qiskit.circuit.QuantumCircuit.html)
     """
 
+    def __init__(self, *args, **kwargs):
+        """Initialize the QuantumOperation class."""
+        self.quantum_circuit = QuantumCircuit(*args, **kwargs)
+
     def to_matrix(self) -> NDArray[np.complex128]:
         """Return matrix form of the quantum circuit as a numpy array.
 
@@ -43,7 +55,7 @@ class QuantumOperation(QuantumCircuit):
         Returns:
             NDArray[np.complex128]: The matrix representation of the quantum circuit.
         """
-        reverse_qc = self.reverse_bits()  # REVERSE the order of qubits to match textbook notation
+        reverse_qc = self.quantum_circuit.reverse_bits()  # REVERSE the order of qubits to match textbook notation
         return np.asarray(quantum_info.Operator(reverse_qc).data, dtype=np.complex128)
 
     def draw(self, output: str | None = "mpl", source: bool = False, *args, **kwargs):
@@ -67,12 +79,12 @@ class QuantumOperation(QuantumCircuit):
                 return latex_drawer.matrix_to_latex(self.to_matrix(), source=source)
             case "latex":
                 if source:
-                    return super().draw(output="latex_source", *args, **kwargs)
-                return super().draw(output="latex", *args, **kwargs)
+                    return self.quantum_circuit.draw(output="latex_source", *args, **kwargs)
+                return self.quantum_circuit.draw(output="latex", *args, **kwargs)
             case _:
-                return super().draw(output=output, *args, **kwargs)
+                return self.quantum_circuit.draw(output=output, *args, **kwargs)
 
-    def _xyz_convert_circ(self, target_basis: str, current_basis: str, qubit_index: int) -> None:
+    def _xyz_convert_circ(self, target_basis: str, current_basis: str, qubit_index: int) -> QuantumOperation:
         """Add the corresponding gate that converts different basis.
 
         This method adds the appropriate gates to the circuit to convert a qubit
@@ -84,23 +96,59 @@ class QuantumOperation(QuantumCircuit):
             qubit_index (int): The index of the qubit to be converted.
         """
         if current_basis == target_basis:
-            return
+            return self
         current_basis += target_basis
         match current_basis:
             case "zx" | "xz":
-                self.h(qubit_index)
+                self.quantum_circuit.h(qubit_index)
             case "zy":
-                self.sdg(qubit_index)
-                self.h(qubit_index)
+                self.quantum_circuit.sdg(qubit_index)
+                self.quantum_circuit.h(qubit_index)
             case "yz":
-                self.h(qubit_index)
-                self.s(qubit_index)
+                self.quantum_circuit.h(qubit_index)
+                self.quantum_circuit.s(qubit_index)
             case "xy":
-                self.h(qubit_index)
-                self.sdg(qubit_index)
-                self.h(qubit_index)
+                self.quantum_circuit.h(qubit_index)
+                self.quantum_circuit.sdg(qubit_index)
+                self.quantum_circuit.h(qubit_index)
             case "yx":
-                self.h(qubit_index)
-                self.s(qubit_index)
-                self.h(qubit_index)
-        return
+                self.quantum_circuit.h(qubit_index)
+                self.quantum_circuit.s(qubit_index)
+                self.quantum_circuit.h(qubit_index)
+        return self
+
+    def unitary(
+        self,
+        matrix: NDArray[np.complex128] | list[list[int]],
+        qubits: Sequence[QubitSpecifier],
+        label: str | None = None,
+    ) -> QuantumOperation:
+        """Apply a unitary matrix to specified qubits.
+
+        This method applies a given unitary matrix to the specified qubits in the
+        quantum circuit. The matrix is converted to match the qubit order of the
+        quantum circuit by reversing the order of qubits.
+
+        Args:
+            matrix (NDArray[np.complex128] | list[list[int]]): The unitary matrix to apply.
+            qubits (Sequence[QubitSpecifier]): The qubits to which the unitary matrix will be applied.
+            label (str | None, optional): An optional label for the unitary gate. Defaults to None.
+
+        Returns:
+            QuantumOperation: Quantum circuit with the applied unitary matrix.
+        """
+        matrix = np.asarray(matrix, dtype=np.complex128)
+        matrix = inverse_tensor(matrix)  # REVERSE the order of qubits to match textbook notation
+        self.quantum_circuit.unitary(matrix, qubits, label=label)
+        return self
+
+    def __getattr__(self, name):
+        original_method = getattr(self.quantum_circuit, name)
+
+        def wrapper(*args, **kwargs):
+            result = original_method(*args, **kwargs)
+            if isinstance(result, InstructionSet):
+                return self
+            raise AttributeError(f"Method {name} not found in QuantumOperation")
+
+        return wrapper
